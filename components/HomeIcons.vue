cc49cbdc1fe70d931387ba655b272307faf55f38
Corentin PERROUX <corentin7301@gmail.com>
2022-11-15 23:55:02 +0100

    send images to assets folder

--- components/HomeIcons.vue
+++ components/HomeIcons.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex items-center justify-between">
-        <nuxt-img :src="`/images/sun/27.png`" sizes="sm:100vw"
+        <nuxt-img :src="`~/assets/images/sun/27.png`" sizes="sm:100vw"
             class="fly-animation max-w-[50%]" />
         <div class="grid gap-4">
-            <nuxt-img :src="`/images/cloud/24.png`" sizes="sm:75vw"
+            <nuxt-img :src="`~/assets/images/cloud/24.png`" sizes="sm:75vw"
                 class="max-w-[140px]" />
-            <nuxt-img :src="`/images/cloud/18.png`" sizes="sm:75vw"
+            <nuxt-img :src="`~/assets/images/cloud/18.png`" sizes="sm:75vw"
                 class="max-w-[140px]" />
-            <nuxt-img :src="`/images/moon/41.png`" sizes="sm:75vw"
+            <nuxt-img :src="`~/assets/images/moon/41.png`" sizes="sm:75vw"
                 class="max-w-[140px]" />
         </div>
     </div>
