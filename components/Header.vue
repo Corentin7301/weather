031c04fb62bf86ee298bb9e03ad1d942b5cada5b
Corentin PERROUX <corentin7301@gmail.com>
2022-11-16 09:37:03 +0100

    add global variables

--- components/Header.vue
+++ components/Header.vue
@@ -3,12 +3,13 @@
         <button @click="returnToSearchbar()" class="p-2 ml-2 rounded-full bg-grad-card-perso opacity-80">
             <Icon name="material-symbols:arrow-right-alt-rounded" size="32px" class="text-white rotate-180" />
         </button>
-        <h1 class="text-xl text-center opacity-80">Wheather App</h1>
+        <h1 class="text-xl text-center opacity-80">{{global.siteName}}</h1>
         <span class="spacer w-[50px]"></span>
     </div>
 </template>
 
 <script setup>
+import global from '~/site.config.json'
     const returnToSearchbar = () => {
         setWeatherDatas(null)
         return navigateTo('/')
