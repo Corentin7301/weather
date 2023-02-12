8d83696a7c615c386be7a0802b96d3b85df1bc88
Corentin PERROUX <corentin7301@gmail.com>
2023-02-12 13:21:11 +0100

    delete umami

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -102,7 +102,7 @@ export default defineNuxtConfig({
     },
     pageTransition: { name: 'page', mode: 'out-in' },
   },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon','@kevinmarrec/nuxt-pwa','@nuxt/content','nuxt-umami','@nuxtjs/plausible',['@funken-studio/sitemap-nuxt-3', { generateOnBuild: true }],'@nuxtjs/robots'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon','@kevinmarrec/nuxt-pwa','@nuxt/content','@nuxtjs/plausible',['@funken-studio/sitemap-nuxt-3', { generateOnBuild: true }],'@nuxtjs/robots'],
   css: ['@/assets/css/tailwind.css', '@/assets/css/fonts.css', '@/assets/css/animations.css',
     '@/assets/css/gradients.css', '@/assets/css/utils.css'
   ],
@@ -139,12 +139,12 @@ export default defineNuxtConfig({
     //   enabled: true
     // },
   },
-  umami: {
-    autoTrack: true,
-    doNotTrack: false,
-    websiteId: '1b9dd955-daba-47aa-a8e9-ad83083e31c1',
-    scriptUrl: 'https://analytics.corentinperroux.fr/umami.js'
-  },
+  // umami: {
+  //   autoTrack: true,
+  //   doNotTrack: false,
+  //   websiteId: '1b9dd955-daba-47aa-a8e9-ad83083e31c1',
+  //   scriptUrl: 'https://analytics.corentinperroux.fr/umami.js'
+  // },
   plausible: {
     apiHost: 'https://analy.corentinperroux.fr',
   },
